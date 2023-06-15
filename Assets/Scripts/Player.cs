f39d11c46f1d80a8d35c389249dca8a02da9178c
tomas96ech <techeverri96@gmail.com>
2023-06-14 22:44:16 -0500

    InitialCommit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Components
+    private Rigidbody2D rb;
+    private Animator playerAnim;
+
+    [Header("Player Movement")]
+    public float movementSpeed;
+    public float jumpForce;
+    
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private float movingInput;
+    private bool isGrounded;
+
+
+    private bool isRunning;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        AnimatorControllers();
+        CheckInput();
+        CheckCollision();
+
+        rb.velocity = new Vector2(movementSpeed * movingInput, rb.velocity.y);
+
+
+    }
+
+    private void AnimatorControllers()
+    {
+        isRunning = rb.velocity.x != 0;
+        playerAnim.SetBool("isRunning", isRunning);
+        playerAnim.SetBool("isGrounded", isGrounded);
+        playerAnim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        movingInput = Input.GetAxis("Horizontal");   
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }     
+    }
+
+  
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+}
