0241f53f2dd002b01a3324ecb7671fe6edb11d90
tomas96ech <techeverri96@gmail.com>
2023-06-15 19:33:36 -0500

    Parallax Effect
    New Background Parallax Effect
    New Night star background

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private GameObject cam;
+
+    [SerializeField] private float parallax;
+
+    private float length;
+    private float xPosition;
+
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        xPosition = transform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        float distanceMoved = cam.transform.position.x * (1 - parallax);
+        float distanceToMove = cam.transform.position.x * parallax;
+
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if (distanceMoved > xPosition + length)
+        {
+            xPosition = xPosition + length;
+        }
+    }
+}
